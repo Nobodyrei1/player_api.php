7543811fc6d01f5e359367cf3bc1815850f9fd62
Nobodyrei1 <115895711+Nobodyrei1@users.noreply.github.com>
2023-09-26 00:20:37 -0300

    Update and rename logar[1].php to logar.php

---
+++ logar.php
@@ -0,0 +1,31 @@
+{
+  "user_info": {
+    "username": "japa",
+    "password": "japa123",
+    "message": null,
+    "auth": 1,
+    "status": "Active",
+    "exp_date": null,
+    "is_trial": "0",
+    "active_cons": 2,
+    "created_at": "1581851239",
+    "max_connections": "1",
+    "allowed_output_formats": [
+      "m3u8",
+      "ts"
+    ]
+  },
+  "server_info": {
+    "xui": true,
+    "version": "1.5.5",
+    "revision": 2,
+    "url": "esrv.cc",
+    "port": "80",
+    "https_port": "443",
+    "server_protocol": "http",
+    "rtmp_port": "8880",
+    "timestamp_now": 1695698394,
+    "time_now": "2023-09-26 00:19:54",
+    "timezone": "America/Sao_Paulo"
+  }
+}
