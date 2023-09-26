4e00cf6811645e6972e3065adcb386857fda22bb
Nobodyrei1 <115895711+Nobodyrei1@users.noreply.github.com>
2023-09-26 00:23:54 -0300

    Add files via upload

---
+++ player_api[1].php
@@ -0,0 +1,30 @@
+{
+  "user_info": {
+    "username": "",
+    "password": "",
+    "message": "Seja bem vindo, seu Alien digdindigdin",
+    "auth": 1,
+    "status": "Active",
+    "exp_date": "1975258257",
+    "is_trial": "0",
+    "active_cons": "0",
+    "created_at": "1530149778",
+    "max_connections": "999",
+    "allowed_output_formats": [
+      "m3u8",
+      "ts",
+      "rtmp"
+    ]
+  },
+  "server_info": {
+    "url": "iptv.movie",
+    "port": "8080",
+    "https_port": "8443",
+    "server_protocol": "https",
+    "rtmp_port": "25462",
+    "timezone": "America/Sao_Paulo",
+    "timestamp_now": "1675258257",
+"time_now": "2023-01-12 13:25:27",
+    "process": true
+  }
+}
