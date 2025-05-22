16c8ad047b41dd502777fb41144a16f0c684fc15
Nobodyrei1 <115895711+Nobodyrei1@users.noreply.github.com>
2025-05-22 13:23:29 -0300

    webapi.php

--- player_api.php
+++ player_api.php
@@ -1,30 +1,14 @@
-{
-  "user_info": {
-    "username": "",
-    "password": "",
-    "message": "Seja bem vindo, seu Alien digdindigdin",
-    "auth": 1,
-    "status": "Active",
-    "exp_date": "1975258257",
-    "is_trial": "0",
-    "active_cons": "0",
-    "created_at": "1530149778",
-    "max_connections": "999",
-    "allowed_output_formats": [
-      "m3u8",
-      "ts",
-      "rtmp"
-    ]
-  },
-  "server_info": {
-    "url": "iptv.movie",
-    "port": "8080",
-    "https_port": "8443",
-    "server_protocol": "https",
-    "rtmp_port": "25462",
-    "timezone": "America/Sao_Paulo",
-    "timestamp_now": "1675258257",
-"time_now": "2023-01-12 13:25:27",
-    "process": true
-  }
+{ 
+   "AdName": "Iron Man", 
+   "AdUrl": "https://i.pinimg.com/originals/31/cb/a7/31cba712a36044f2afa7d360fbada5b3.gif" 
+  }, 
+  { 
+   "AdName": "Avatar 2", 
+   "AdUrl": "https://www.icegif.com/wp-content/uploads/cool-wallpapers-icegif-1.gif" 
+  }, 
+  { 
+   "AdName": "Last of Us", 
+   "AdUrl": "https://wallpaperaccess.com/full/54673.jpg" 
+  } 
+ ] 
 }
