25868639e522b1e8130714d17e3c91091bc3a87b
Nobodyrei1 <115895711+Nobodyrei1@users.noreply.github.com>
2025-05-22 13:24:31 -0300

    webapp.php

---
+++ webapp.php
@@ -0,0 +1,14 @@
+{ 
+   "AdName": "Iron Man", 
+   "AdUrl": "https://i.pinimg.com/originals/31/cb/a7/31cba712a36044f2afa7d360fbada5b3.gif" 
+  }, 
+  { 
+   "AdName": "Avatar 2", 
+   "AdUrl": "https://www.icegif.com/wp-content/uploads/cool-wallpapers-icegif-1.gif" 
+  }, 
+  { 
+   "AdName": "Last of Us", 
+   "AdUrl": "https://wallpaperaccess.com/full/54673.jpg" 
+  } 
+ ] 
+}
